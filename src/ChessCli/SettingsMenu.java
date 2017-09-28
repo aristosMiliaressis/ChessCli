6917a3d7da603661df5783645a97235d15d598f4
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2017-09-28 07:41:56 +0300

    Create SettingsMenu.java

---
+++ src/ChessCli/SettingsMenu.java
@@ -0,0 +1,19 @@
+package ChessCli;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class SettingsMenu extends JPanel {
+
+    /**
+     * Create the panel.
+     */
+    public SettingsMenu() {
+        setBorder(new EmptyBorder(5, 5, 5, 5));
+        
+        BoxLayout bl_contentPane = new BoxLayout(this, BoxLayout.Y_AXIS);
+        setLayout(bl_contentPane);
+    }
+
+}
