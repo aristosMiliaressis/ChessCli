ec730c2693b44d5969f391b0d2ec3d0bb8bd61bb
Aristos Miliaresis <aristosmiliaresis@gmail.com>
2017-09-28 07:44:39 +0300

    Create Piece.java

---
+++ src/ChessCore/Piece.java
@@ -0,0 +1,111 @@
+package ChessCore;
+
+import java.util.HashMap;
+
+public abstract class Piece {
+	public enum Color {
+		WHITE {
+		    @Override public String toString() {
+		        return "White";
+		    }
+		},
+		BLACK {
+	        @Override public String toString() {
+	            return "Black";
+	        }
+	    }
+	};
+
+	public enum Type {
+		PAWN {
+		    @Override public String toString() {
+                return "Pawn";
+            }
+        },
+		ROCK {
+            @Override public String toString() {
+                return "Rock";
+            }
+        },
+		KNIGHT {
+            @Override public String toString() {
+                return "Knight";
+            }
+        },
+		BISHOP {
+            @Override public String toString() {
+                return "Bishop";
+            }
+        },
+		QUEEN {
+            @Override public String toString() {
+                return "Queen";
+            }
+        },
+		KING {
+            @Override public String toString() {
+                return "King";
+            }
+        }
+	};
+
+	private Color color;
+	private Type type;
+	private String image;
+	private float value;
+	private Board.Coords coords;
+
+	Piece(Color color, Board.Coords coords) {
+		setCoords(new Board.Coords(coords));
+		this.color = color;
+	}
+
+	public abstract HashMap<Integer, Move> calculateLegalMoves();
+	
+	protected Move createMove() {
+	    Move move = new Move();
+	    Move.Descriptor descriptor = move.new Descriptor();
+	    
+	    descriptor.setSrcCoords(getCoords());
+        descriptor.playingColor(getColor());
+        
+        move.setDescriptor(descriptor);
+        return move;
+	}
+    
+    public Color getColor() {
+        return color;
+    }
+    
+    protected void setType(Type type) {
+    	this.type = type;
+    }
+
+    public Type getType() {
+    	return type;
+    }
+
+    protected void setCoords(Board.Coords coords) {
+    	this.coords = coords;
+    }
+
+    public Board.Coords getCoords() {
+    	return coords;
+    }
+    
+    protected void setValue(float value) {
+    	this.value = value;
+    }
+    
+    public float getValue() {
+    	return value;
+    }
+    
+    protected void setImageFN(String image) {
+        this.image = image;
+    }
+    
+    public String getImageFN() {
+        return image;
+    }
+}
